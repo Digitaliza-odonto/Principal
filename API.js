d84a9e906222105610efaff6d6a4d38dceee29d0
pedroaf0 <pedroaf1233@gmail.com>
2023-10-04 11:11:06 -0300

    move api.js

--- API.js
+++ API.js
@@ -1,6 +1,6 @@
 const getApiUrl = () => {
   const storedApiUrl = localStorage.getItem('api');
-  return storedApiUrl ? storedApiUrl : 'https://ruufpelbot.000webhostapp.com/Prontuario-ufpel/prontuario-ufpel-php-main/api/index.php?rota=';
+  return storedApiUrl ? storedApiUrl : '/API';
 };
 
 const sendRequest = async (url, method, data) => {
@@ -26,62 +26,62 @@ const sendRequest = async (url, method, data) => {
 
 const pacientes = {
   criar: async (data) => {
-    const url = `${getApiUrl()}/pacientes/criar`;
+    const url = `${getApiUrl()}/pacientes/criar.php`;
     return sendRequest(url, 'POST', data);
   },
   consultar: async (data) => {
-    const url = `${getApiUrl()}/pacientes/consultar`;
+    const url = `${getApiUrl()}/pacientes/consultar.php`;
     return sendRequest(url, 'POST', data);
   },
   atualizar: async (data) => {
-    const url = `${getApiUrl()}/pacientes/atualizar`;
+    const url = `${getApiUrl()}/pacientes/atualizar.php`;
     return sendRequest(url, 'POST', data);
   }
 };
 
 const encaminhamentos = {
   criar: async (data) => {
-    const url = `${getApiUrl()}/encaminhamentos/criar`;
+    const url = `${getApiUrl()}/encaminhamentos/criar.php`;
     return sendRequest(url, 'POST', data);
   },
   consultar: async (data) => {
-    const url = `${getApiUrl()}/encaminhamentos/consultar`;
+    const url = `${getApiUrl()}/encaminhamentos/consultar.php`;
     return sendRequest(url, 'POST', data);
   },
   atualizar: async (data) => {
-    const url = `${getApiUrl()}/encaminhamentos/atualizar`;
+    const url = `${getApiUrl()}/encaminhamentos/atualizar.php`;
     return sendRequest(url, 'POST', data);
   },
 };
 
 const usuarios = {
 login: async (data) => {
-  const url = `${getApiUrl()}/usuarios/login`;
+  const url = `${getApiUrl()}/usuarios/login.php`;
   return sendRequest(url, 'POST', data);
 },
 criar: async (data) => {
-  const url = `${getApiUrl()}/usuarios/criar`;
+  const url = `${getApiUrl()}/usuarios/criar.php`;
   return sendRequest(url, 'POST', data);
 }
 }
 const alunos = {
   pacientes: async (data) => {
-    const url = `${getApiUrl()}/alunos/pacientes`;
+    const url = `${getApiUrl()}/alunos/pacientes.php`;
     return sendRequest(url, 'POST', data);
   },
   vincular: async (data) => {
-    const url = `${getApiUrl()}/alunos/vincular`;
+    const url = `${getApiUrl()}/alunos/vincular.php`;
     return sendRequest(url, 'POST', data);
   }
 };
 
 const disciplinas = {
   criar: async (data) => {
-    const url = `${getApiUrl()}/disciplinas/criar`;
+    const url = `${getApiUrl()}/disciplinas/criar.php`;
     return sendRequest(url, 'POST', data);
   },
   consultar: async (data) => {
-    const url = `${getApiUrl()}/disciplinas/consultar`;
+    const url = `${getApiUrl()}/disciplinas/consultar.php`;
     return sendRequest(url, 'POST', data);
   }
 };
@@ -93,3 +93,17 @@ window.api = {
   alunos,
   disciplinas
 };
+
+// remove o banner do 000webhost
+function adicionarEstilo() {
+  // Crie um elemento <style> e configure o CSS desejado
+  var estilo = document.createElement('style');
+  estilo.textContent = 'img[alt="www.000webhost.com"] { display: none; }';
+
+  // Adicione o elemento <style> ao cabeçalho (head) do documento
+  var head = document.head || document.getElementsByTagName('head')[0];
+  head.appendChild(estilo);
+}
+
+// Chame a função para adicionar o estilo à página
+adicionarEstilo();

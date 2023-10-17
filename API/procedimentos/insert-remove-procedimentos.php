14a3c5ea207fdd50fc2076bb3b669fb28e9b6382
Mateus Bertolini Fernandes dos Santos <138033953+Mateusbertolini@users.noreply.github.com>
2023-10-16 22:40:31 -0300

    AJAX -> API

--- API/procedimentos/insert-remove-procedimentos.php
+++ API/procedimentos/insert-remove-procedimentos.php
@@ -1,75 +1,45 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
 include_once('../config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['selectedId']) && is_array($_POST['selectedId'])) {
-        // Check if 'selectedId' is present; if so, it's a delete operation
-        $selectedIds = $_POST['selectedId'];
+    // Read the raw POST data as JSON
+    $json_data = json_decode(file_get_contents("php://input"), true);
 
-        // Loop through the selected IDs and delete the corresponding records
-        $successMessage = "";
-        $errorMessage = "";
+    // Check if required fields are present in the JSON data
+    if (
+        isset($json_data['codSUS']) &&
+        isset($json_data['DescricaoSUS']) &&
+        isset($json_data['tipoSUS']) &&
+        $json_data['tipoSUS'] !== "Escolher"
+    ) {
+        // Retrieve data from JSON
+        $codSUS = $json_data['codSUS'];
+        $descricaoSUS = $json_data['DescricaoSUS'];
+        $tipoSUS = $json_data['tipoSUS'];
 
-        foreach ($selectedIds as $idToDelete) {
-            // Perform the database deletion
-            $sql = "DELETE FROM `procedimentos_sus` WHERE `cod_sus` = ?";
-            $stmt = $connection->prepare($sql);
-            $stmt->bind_param("i", $idToDelete);
+        // Perform database insertion
+        $sql = "INSERT INTO `procedimentos_sus` (`cod_sus`, `nome`, `tipo`) VALUES (?, ?, ?)";
+        $stmt = $connection->prepare($sql);
+        $stmt->bind_param("iss", $codSUS, $descricaoSUS, $tipoSUS);
 
-            if ($stmt->execute()) {
-                $successMessage = "Procedimento(s) deletado(s) com sucesso.";
-            } else {
-                $errorMessage = "Erro ao deletar procedimento: " . $stmt->error;
-            }
-
-            // Close the prepared statement
-            $stmt->close();
-        }
-
-        // Return the response as JSON
-        if ($errorMessage !== "") {
-            echo json_encode($errorMessage);
-        } else {
-            echo json_encode($successMessage);
-        }
-    } else {
-        // If 'selectedId' is not present, it's an insert or update operation
-        // Check if the form fields are set and meet the required conditions
-        if (
-            isset($_POST['codSUS']) && 
-            isset($_POST['descricaoSUS']) && 
-            isset($_POST['tipoSUS']) && 
-            $_POST['tipoSUS'] !== "Escolher"
-        ) {
-            // Retrieve form data
-            $codSUS = $_POST['codSUS'];
-            $descricaoSUS = $_POST['descricaoSUS'];
-            $tipoSUS = $_POST['tipoSUS'];
-
-            // It's an insert operation as 'codSUS' is provided by the user
-            $sql = "INSERT INTO `procedimentos_sus` (`cod_sus`, `nome`, `tipo`) VALUES (?, ?, ?)";
-            $stmt = $connection->prepare($sql);
-            $stmt->bind_param("iss", $codSUS, $descricaoSUS, $tipoSUS);
-
-            if ($stmt->execute()) {
-                // Return success message as JSON
-                echo json_encode("Procedimento cadastrado com sucesso.");
-            } else {
-                // Return error message as JSON
-                echo json_encode("Erro ao cadastrar procedimento: " . $stmt->error);
-            }
-
-            // Close the prepared statement
-            $stmt->close();
+        if ($stmt->execute()) {
+            // Return success message as JSON
+            echo json_encode("Procedimento cadastrado com sucesso.");
         } else {
             // Return error message as JSON
-            echo json_encode("Um ou mais campos do formulário estão faltando ou não atendem às condições necessárias.");
+            echo json_encode("Erro ao cadastrar procedimento: " . $stmt->error);
         }
+
+        // Close the prepared statement
+        $stmt->close();
+    } else {
+        // Return error message as JSON
+        echo json_encode("Um ou mais campos do formulário estão faltando ou não atendem às condições necessárias.");
     }
 } else {
     // Handle the case where the form was not submitted

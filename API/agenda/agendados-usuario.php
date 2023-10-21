8302ac6c9de0f00009516aa1ea843495844c6036
pedroaf0 <pedroaf1233@gmail.com>
2023-10-21 11:54:36 -0300

    arualizar agendados usuário

--- API/agenda/agendados-usuario.php
+++ API/agenda/agendados-usuario.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods: GET, POST, PUT");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-include_once('../config.php');
+include_once('../db.php');
 
 // Get the JSON data from the request body
 $jsonData = file_get_contents('php://input');
@@ -16,31 +16,22 @@ if (isset($data->id)) {
     $id = $data->id;
 
     // Use prepared statement to avoid SQL injection
-    $stmt = $connection->prepare("SELECT * FROM `agenda-clinicas` WHERE `agendado_por` = ? AND (`status_agendamento` = 'Agendado' OR `status_agendamento` = 'No local')");
-    $stmt->bind_param("s", $id);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $query = "SELECT * FROM `agenda-clinicas` WHERE `agendado_por` = $id AND (`status_agendamento` = 'Agendado' OR `status_agendamento` = 'No local')";
+
+    $result = db($query);
 
     if ($result) {
-        while ($linha = $result->fetch_assoc()) {
-            $responseData[] = $linha;
-        }
-
         // Output the data as JSON
-        echo json_encode($responseData);
+        echo json_encode($result);
     } else {
         // Handle the error gracefully and return an error JSON response
-        $error = array("error" => "Database query error: " . $connection->error);
+        $error = array("error" => "Database query error: ");
         echo json_encode($error);
     }
-
-    // Close the prepared statement and the database connection
-    $stmt->close();
 } else {
     // Return an error JSON response if 'id' key is not present in the JSON data
     $error = array("error" => "Invalid or missing 'id' parameter in the request body");
     echo json_encode($error);
 }
 
-$connection->close();
 ?>

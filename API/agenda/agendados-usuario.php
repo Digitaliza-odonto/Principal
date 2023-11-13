31e17bbb6a23253352001abd464bf1bce0f3e585
Mateus Bertolini Fernandes dos Santos <138033953+Mateusbertolini@users.noreply.github.com>
2023-11-13 14:10:19 -0300

    Ajuste de layout e funcionalidades

--- API/agenda/agendados-usuario.php
+++ API/agenda/agendados-usuario.php
@@ -1,37 +1,41 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
-include_once('../db.php');
+include_once '../db.php';
 
-// Get the JSON data from the request body
-$jsonData = file_get_contents('php://input');
-$data = json_decode($jsonData);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $id = $data['id'];
 
-// Check if 'id' key exists in the JSON data
-if (isset($data->id)) {
-    // Get the value of 'id' from the JSON data
-    $id = $data->id;
+    // Fetch vinculos
+    $query = db("SELECT * FROM `agenda-clinicas` WHERE `agendado_por` = '$id' AND (`status_agendamento` = 'Agendado' OR `status_agendamento` = 'No local')");
 
-    // Use prepared statement to avoid SQL injection
-    $query = "SELECT * FROM `agenda-clinicas` WHERE `agendado_por` = '$id' AND (`status_agendamento` = 'Agendado' OR `status_agendamento` = 'No local')";
+    // Check if there are vinculos
+    if (!empty($query)) {
+        $pacienteInfoList = array();
 
-    $result = db($query);
+        // Loop through each vinculo
+        foreach ($query as $queryCPF) {
+            // Extract CPF_paciente from the queryCPF
+            $cpf_paciente = $queryCPF['cpf_paciente'];
 
-    if ($result) {
-        // Output the data as JSON
-        echo json_encode($result);
+            // Perform the second SQL operation for each 'cpf_paciente'
+            $pacienteInfo = db("SELECT `nome` FROM `pacientes` WHERE CPF = '$cpf_paciente'");
+
+            // Add the result to the list
+            $pacienteInfoList[] = $pacienteInfo;
+        }
+
+        // Include vinculos, pacienteInfoList, and TurmaInfo in the JSON response
+        echo json_encode(array("vinculos" => $query, "pacienteInfoList" => $pacienteInfoList));
     } else {
-        // Handle the error gracefully and return an error JSON response
-        $error = array("error" => "Database query error: ");
-        echo json_encode($error);
+        echo json_encode(array("message" => "No vinculos found"));
     }
 } else {
-    // Return an error JSON response if 'id' key is not present in the JSON data
-    $error = array("error" => "Invalid or missing 'id' parameter in the request body");
-    echo json_encode($error);
+    echo json_encode(array("message" => "Método inválido"));
 }
-
 ?>
